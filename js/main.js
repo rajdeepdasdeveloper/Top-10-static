ed50ab965df3c7e696fe441d6d30fee8c6a2f660
rajdeepdas47 <rajdeep.das.@innoraft.com>
2018-01-15 16:19:00 +0530

    Details Page

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
-$(document).ready(function(){ // Ready Start
+$(document).on("ready", function(){ // Ready Start
 	
-	
+	$("#top-10-list .side-bar").css(
+		'margin-top', 
+		(($("#top-10-list .block-list-title").height() + 15) - $("#top-10-list .side-bar .side-bar-heading").height()) + "px"
+	);
 	
 }); // Ready End
+
+$(window).on("load", function(){ // Load Start
+	
+	
+}); // Load End
